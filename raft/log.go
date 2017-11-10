ba07ed23c9f9d6079a33e11ff7ec127c6b949e78
Jacob Martin <jakub.wit.martin@gmail.com>
2017-11-10 23:25:09 +0100

    Continued

--- raft/log.go
+++ raft/log.go
@@ -14,32 +14,35 @@ type entryLog struct {
 	mutex sync.RWMutex
 }
 
-func NewEntryLog() *entryLog {
+func NewEntryLog() (*entryLog, error) {
 	l := &entryLog{
 		log: make([]raft.Entry, 0),
 	}
 
-	// TODO: Do fstat, then potentially open with error checking
-	file, _ := os.Open("log.json")
+	file, err := os.Open("log.json")
+	if !os.IsNotExist(err) {
+		return nil, errors.Wrap(err, "Couldn't open persisted commit log")
+	}
 	if file != nil {
 		err := json.NewDecoder(file).Decode(&l.log)
 		if err != nil {
-			// TODO: Proper error handling
-			log.Fatalf("File corrupted: %v", err)
+			return nil, errors.Wrap(err, "Couldn't decode commit log file, file corrupted")
 		}
 	}
 
-	return l
+	return l, nil
 }
 
-func (l *entryLog) persist() {
+func (l *entryLog) persist() error {
 	file, err := os.Create("log.json")
 	if err != nil {
-		// TODO: Proper error handling
-		log.Fatalf("Why doesn't file io work :( ? %v", err)
+		return errors.Wrap(err, "Couldn't create file to persist commit log")
 	}
 	defer file.Close()
-	json.NewEncoder(file).Encode(l.log)
+	if err = json.NewEncoder(file).Encode(l.log); err != nil {
+		return errors.Wrap(err, "Couldn't encode commit log to persistent storage")
+	}
+	return nil
 }
 
 func (l *entryLog) Exists(index int64, term int64) bool {
@@ -76,27 +79,35 @@ func (l *entryLog) Get(index int64) (*raft.Entry, error) {
 	return &l.log[index], nil
 }
 
-func (l *entryLog) DeleteFrom(index int64) {
+func (l *entryLog) DeleteFrom(index int64) error {
 	l.mutex.Lock()
 	defer l.mutex.Unlock()
 
 	index = index - 1
 	if int64(len(l.log)) <= index {
-		return
+		return nil
 	}
 
 	l.log = l.log[:index]
-	l.persist()
+	return errors.Wrap(
+		l.persist(),
+		"Couldn't persist deletion of entries from given index",
+	)
 }
 
-func (l *entryLog) Append(entry *raft.Entry, term int64) int64 {
+func (l *entryLog) Append(entry *raft.Entry, term int64) (int64, error) {
 	l.mutex.Lock()
 	defer l.mutex.Unlock()
 
 	l.log = append(l.log, *entry)
-	l.persist()
+	err := l.persist()
+	if err != nil {
+		// If there's en error when persisting, undo adding that entry
+		l.log = l.log[:len(l.log)-1]
+		return -1, errors.Wrap(err, "Couldn't persist log")
+	}
 	// Keep in mind, indexes are from 1.
-	return int64(len(l.log))
+	return int64(len(l.log)), nil
 }
 
 func (l *entryLog) MaxIndex() int64 {
