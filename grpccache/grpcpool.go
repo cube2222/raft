3187eb7ae92bada344a8c075890dd7facb3f346c
Jakub Martin <jakub.wit.martin@gmail.com>
2017-11-19 14:46:10 +0100

    Second attempt

--- grpccache/grpcpool.go
+++ grpccache/grpcpool.go
@@ -2,19 +2,18 @@ package grpccache
 
 import (
 	"context"
-	"sync"
 
 	"github.com/pkg/errors"
 	"google.golang.org/grpc"
 )
 
 type Cache struct {
-	connsMutex  sync.RWMutex
 	connections map[address]*grpc.ClientConn
-	requests    <-chan *connectionRequest
+	requests    chan *connectionRequest
 }
 
 type connectionRequest struct {
+	ctx          context.Context
 	address      address
 	responseChan chan<- *finishedConnection
 }
@@ -27,35 +26,31 @@ type finishedConnection struct {
 	err        error
 }
 
-func (cache *Cache) GetConnection(ctx context.Context, address address) (*grpc.ClientConn, error) {
-	cache.connsMutex.RLock()
-	conn, ok := cache.connections[address]
-	cache.connsMutex.RUnlock()
-	if ok {
-		return conn, nil
+func NewCache(ctx context.Context) *Cache {
+	cache := &Cache{
+		connections: make(map[address]*grpc.ClientConn),
+		requests:    make(chan *connectionRequest),
 	}
+	go cache.loop(ctx)
 
+	return cache
+}
+
+func (cache *Cache) GetConnection(ctx context.Context, address address) (*grpc.ClientConn, error) {
 	responseChan := make(chan *finishedConnection)
 
-	cache.requests <- &connectionRequest{
+	select {
+	case cache.requests <- &connectionRequest{
+		ctx:          ctx,
 		address:      address,
 		responseChan: responseChan,
+	}:
+	case <-ctx.Done():
+		return nil, ctx.Err()
 	}
 
 	select {
 	case res := <-responseChan:
-		if res == nil {
-			// This means we were too late to listen, had been ignored and received nil.
-			// But the connection should now be in the connection cache, so we can just take it from there.
-			cache.connsMutex.RLock()
-			conn, ok := cache.connections[address]
-			cache.connsMutex.RUnlock()
-			if !ok {
-				return nil, errors.New("Got nil connection, this really shouldn't happen. Concurrency Bug.")
-			}
-
-			return conn, nil
-		}
 		return res.connection, res.err
 	case <-ctx.Done():
 		return nil, ctx.Err()
@@ -63,23 +58,23 @@ func (cache *Cache) GetConnection(ctx context.Context, address address) (*grpc.C
 }
 
 func (cache *Cache) loop(ctx context.Context) {
-	waiting := make(map[address][]chan<- *finishedConnection)
+	waiting := make(map[address][]*connectionRequest)
 	finished := make(chan *finishedConnection)
+
 	for {
 		select {
 		case req := <-cache.requests:
 			// Just send the connection over if it exists
-			cache.connsMutex.RLock()
-			conn, ok := cache.connections[req.address]
-			cache.connsMutex.RUnlock()
-			if ok {
+			if conn, ok := cache.connections[req.address]; ok {
 				// This sends the connection over if the client is waiting on the other side. Aborts otherwise.
+				// This also aborts if the cache context gets cancelled.
 				select {
 				case req.responseChan <- &finishedConnection{
 					address:    req.address,
 					connection: conn,
 				}:
-				default:
+				case <-req.ctx.Done():
+				case <-ctx.Done():
 				}
 				close(req.responseChan)
 				continue
@@ -88,28 +83,33 @@ func (cache *Cache) loop(ctx context.Context) {
 			// If there are already existing clients waiting for this connection, then a worker thread
 			// creating it is in progress already, just add this client as another one waiting for it.
 			if alreadyWaiting, ok := waiting[req.address]; ok {
-				waiting[req.address] = append(alreadyWaiting, req.responseChan)
+				waiting[req.address] = append(alreadyWaiting, req)
 				continue
 			}
 
 			// If there is no one already waiting, start a new worker to create this connection.
-			waiting[req.address] = [](chan<- *finishedConnection){req.responseChan}
+			waiting[req.address] = []*connectionRequest{req}
 			go buildConnection(ctx, req.address, finished)
+
 		case conn := <-finished:
 			if conn.err == nil {
-				cache.connsMutex.Lock()
 				cache.connections[conn.address] = conn.connection
-				cache.connsMutex.Unlock()
 			}
 
 			for _, client := range waiting[conn.address] {
-				// Send it over if the client is still there, otherwise ignore him and close the channel.
+				// Send it over if the client is still there. Abort otherwise.
+				// This also aborts if the cache context gets cancelled.
 				select {
-				case client <- conn:
-				default:
+				case client.responseChan <- conn:
+				case <-client.ctx.Done():
+				case <-ctx.Done():
 				}
-				close(client)
+				close(client.responseChan)
 			}
+			delete(waiting, conn.address)
+
+		case <-ctx.Done():
+			return
 		}
 	}
 }
@@ -119,7 +119,7 @@ func buildConnection(ctx context.Context, address address, finished chan<- *fini
 	if err != nil {
 		finished <- &finishedConnection{
 			address: address,
-			err:     errors.Wrapf(err, "Couldn't dial grpc. Address: %v", address),
+			err:     errors.Wrapf(err, "Couldn't dial grpc. Address: %s", address),
 		}
 		return
 	}
